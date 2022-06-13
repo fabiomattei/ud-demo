a15df5442a1894b74c3c4e97e8c5b5afd00fdc3d
Fabio Mattei <burattino@gmail.com>
2022-06-13 19:44:43 +0200

    adding block name

--- src/HTMLBlocks/HTMLBlockExample.php
+++ src/HTMLBlocks/HTMLBlockExample.php
@@ -5,7 +5,9 @@ namespace Fabiom\UDDemo\Custom\HTMLBlocks;
 use Fabiom\UglyDuckling\Common\Blocks\BaseHTMLBlock;
 
 class HTMLBlockExample extends BaseHTMLBlock {
-
+	
+    const HTML_BLOCK_NAME = 'basehtmlblockexample';
+		
     function getHTML(): string {
         return '<p>Paragraph example</p>';
     }
